1ae721278e0aabef32e54b4e92bba8ef716aa443
dieselpoweredkitten <me@require.pm>
2014-01-31 12:00:27 +0400

    Release

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@ setup(
   name = 'beretta',
   author = 'beretta contributors',
   author_email = 'github@require.pm',
-  version = '0.1',
-  description = 'BERT (de)serializer from your Pythons',
+  url = 'https://github.com/dieselpoweredkitten/beretta',
+  version = '0.2',
+  description = 'BERT (de)serializer for your Pythons',
   license = 'MIT',
   ext_modules = [Extension('beretta', ['beretta.c'])],
   classifiers = (
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
+  ),
+  install_requires = (
+    'termformat>=0.1',
   )
 )
